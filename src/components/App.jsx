6e973f1d75cde95bf1bad8552ac636fbc7dd22d3
Anjana Shaji <anjanashaji@example.com>
2026-02-14 20:35:08 +0530

    ci test added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,7 +22,7 @@ function App() {
   return (
     <div className="container">
       <div className="heading">
-        <h1>To-Do List</h1>
+        <h1>To-Do List WebApplication </h1>
       </div>
       <InputArea onAdd={addItem} />
       <div>
